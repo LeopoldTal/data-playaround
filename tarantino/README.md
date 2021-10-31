8738078dc240220a56468e4fc98a40e2736b914b
Leopold Tal G <leopold.tal.dg@gmail.com>
2021-10-31 14:10:24 +0100

    fit Zipf-Mandelbrot

--- tarantino/README.md
+++ tarantino/README.md
@@ -2,7 +2,7 @@
 
 ![Quentinen and Tarantined by Writtin Directino](credits.jpg)
 
-Inexplicably, 538 has a [tally of swears and deaths](https://fivethirtyeight.com/features/complete-catalog-curses-deaths-quentin-tarantino-films/) across all Quentin Tarantino movies. It takes over ten minutes to [say the full list of swears](poetry.mp3).
+Inexplicably, 538 has a [tally of swears and deaths](https://fivethirtyeight.com/features/complete-catalog-curses-deaths-quentin-tarantino-films/) across seven Quentin Tarantino movies. It takes over ten minutes to [say the full list of swears](poetry.mp3).
 
 ## What swears are used?
 
@@ -36,6 +36,16 @@ However, the fit is still poor. Excluding swears used once yields similar result
 
 ### Fitting an empirical distribution
 
-It turns out that the rank is proportional to the square log of the frequency. What could produce this? Search me.
+The [Zipf-Mandelbrot](https://en.wikipedia.org/wiki/Zipf%E2%80%93Mandelbrot_law) law is sometimes used to model a flatter distribution at the top ranks, just like we observe in this dataset. Fitting such a law yields
+
+```
+frequency ∝ (rank + 7.97)^-3.63
+```
+
+![Distribution of all swears and Zipf_Mandelbrot fit](zm_transform.png) ![Same in log scale](zm_log.png)
+
+A pretty good fit! But there are 3 free parameters, and still some systematic deviation at low rank, so I suspect overfitting.
+
+After trying to fit any random relations I can come up with, it turns out that the rank is proportional to the square log of the frequency. What could produce this? Search me.
 
 ![Swear rank vs square log of relative frequency](custom_fit.png)
--- tarantino/tarantino.R
+++ tarantino/tarantino.R
@@ -18,15 +18,18 @@ swears <- subset(tarantino, type == 'word') %>%
 	summarise(count = n()) %>%
 	arrange(desc(count))
 swears$rank <- 1 : nrow(swears)
+swears$rel_freq <- swears$count / sum(swears$count)
+non_unique_swears <- subset(swears, count > 1)
 
 # Most-used swears
 nb_top_swears <- 6
-top_swears <- slice_max(swears, swears$count, n = nb_top_swears)
-other_count <- filter(swears, !(word %in% top_swears$word))$count %>% sum
-top_swears <- rbind(top_swears, data.frame(word = 'other', count = other_count, rank = nb_top_swears + 1))
-top_swears <- top_swears %>% mutate(percentage = 100 * count / sum(count))
+top_swears <- slice_max(swears, swears$count, n = nb_top_swears) %>% select(word, count, rank)
 
-ggplot(top_swears, aes(x = 'Swear', y = percentage, fill = factor(rank))) +
+other_count <- filter(swears, !(word %in% top_swears$word))$count %>% sum
+binned_swears <- rbind(top_swears, data.frame(word = 'other', count = other_count, rank = nb_top_swears + 1))
+binned_swears <- binned_swears %>% mutate(percentage = 100 * count / sum(count))
+
+ggplot(binned_swears, aes(x = 'Swear', y = percentage, fill = factor(rank))) +
 	geom_col(width = 1) +
 	geom_text(
 		aes(label = paste(word, ' (', round(percentage, 1), '%)', sep = '')),
@@ -50,7 +53,7 @@ ggplot(top_swears, aes(x = 'Swear', y = percentage, fill = factor(rank))) +
 ggplot(swears, aes(x = rank, y = count, fill = rank)) +
 	geom_col(width = 1) +
 	scale_fill_viridis_c(option = 'plasma') +
-	scale_x_continuous( 
+	scale_x_continuous(
 		expand = expansion(mult = 0)
 	) +
 	scale_y_continuous(
@@ -65,16 +68,12 @@ ggplot(swears, aes(x = rank, y = count, fill = rank)) +
 		panel.grid.major.x = element_blank(),
 	)
 
-# More stats
-swears$rel_freq <- swears$count / sum(swears$count)
-non_unique_swears <- subset(swears, count > 1)
-
 # Fit Zipf
 ggplot(swears, aes(x = log10(rank), y = log10(rel_freq), color = rank)) +
 	geom_smooth(method = 'lm', data = non_unique_swears) +
 	geom_point() +
 	scale_color_viridis_c(option = 'plasma') +
-	scale_x_continuous( 
+	scale_x_continuous(
 		expand = expansion(mult = c(0.01, 0.05))
 	) +
 	scale_y_continuous(
@@ -104,7 +103,7 @@ ggplot(swears) +
 	geom_point(aes(x = log10(rank), y = log10(rel_freq), color = rank)) +
 	scale_color_viridis_c(option = 'plasma') +
 	geom_line(aes(x = log10(rank), y = log10(yule(rank, best_param))), color = 'darkgreen') +
-	scale_x_continuous( 
+	scale_x_continuous(
 		expand = expansion(mult = c(0.01, 0.05))
 	) +
 	scale_y_continuous(
@@ -143,3 +142,51 @@ ggplot(swears, aes(x = rank, y = log10(rel_freq)**2, color = rank)) +
 		axis.title.y = element_text(angle = 0, margin = margin(r = -4.5, unit = 'cm')),
 	)
 
+# Zipf-Mandelbrot
+get_zm_square_error <- function(param) {
+	y <- log10(non_unique_swears$rel_freq)
+	x <- log10(non_unique_swears$rank + param)
+	fit <- lm(y ~ x)
+	sum(fit$residuals**2)
+}
+
+params_to_try <- seq(0, 50, 0.01)
+best_param <- params_to_try[which.min(sapply(params_to_try, get_zm_square_error))]
+
+ggplot(swears, aes(x = log10(rank + best_param), y = log10(rel_freq), color = rank)) +
+	geom_smooth(method = 'lm', data = non_unique_swears) +
+	geom_point() +
+	scale_color_viridis_c(option = 'plasma') +
+	scale_x_continuous(
+		expand = expansion(mult = c(0.01, 0.05))
+	) +
+	scale_y_continuous(
+		expand = expansion(mult = c(0.01, 0.1))
+	) +
+	labs(title = 'Overall distribution of Tarantinian swears\nZipf-Mandelbrot fit') +
+	xlab(paste('log10(swear rank + ', best_param, ')', sep = '')) +
+	ylab('log10(relative frequency)') +
+	bw_theme +
+	theme(
+		axis.title.y = element_text(angle = 0, margin = margin(r = -4, unit = 'cm')),
+	)
+
+zm_fit <- lm(log10(non_unique_swears$rel_freq) ~ log10(non_unique_swears$rank + best_param))
+zm_expected_freq <- log10(swears$rank + best_param)*zm_fit$coefficients[2] + zm_fit$coefficients[1]
+ggplot(swears) +
+	geom_point(aes(x = log10(rank), y = log10(rel_freq), color = rank)) +
+	scale_color_viridis_c(option = 'plasma') +
+	geom_line(aes(x = log10(rank), y = zm_expected_freq), color = 'darkgreen') +
+	scale_x_continuous(
+		expand = expansion(mult = c(0.01, 0.05))
+	) +
+	scale_y_continuous(
+		expand = expansion(mult = c(0.01, 0.1))
+	) +
+	labs(title = 'Overall distribution of Tarantinian swears\nZipf-Mandelbrot fit') +
+	xlab('Swear rank (log)') +
+	ylab('Relative frequency (log)') +
+	bw_theme +
+	theme(
+		axis.title.y = element_text(angle = 0, margin = margin(r = -3.5, unit = 'cm')),
+	)
